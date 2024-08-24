2519b27b2da50813c43911b620cd210a8319164b
unknown <amjbu05375@gmail.com>
2024-08-24 17:41:56 +0300

    first commit

---
+++ lib/core/apis/apis_url.dart
@@ -0,0 +1,13 @@
+// core/apis/apis_url.dart
+class ApiUrl {
+  static const String BASE_URL = "https://robbikya.onrender.com/api/v1";
+  static const String logIn = "$BASE_URL//auth/login/";
+  static const String category = "/categories/";
+  static const String product = "/products/";
+  static const String subCategory = "/subcategories/";
+  static const String user = "/users/";
+  static const String getCategory = "$BASE_URL/categories/";
+  static const String getSubCategory = "$BASE_URL/subcategories/";
+  static const String getProduct = "$BASE_URL/products/";
+  static const String getUser = "$BASE_URL/users/";
+}
---
+++ lib/core/errors/exception.dart
@@ -0,0 +1,7 @@
+// core/errors/exception.dart
+class ServerException implements Exception {}
+class EmptyCashException implements Exception {}
+class OffLineException implements Exception {}
+class UnAuthorizedException implements Exception {}
+
+
